5be2d3411ca7072c3dd898fa441a013db85b03cd
Prasant Adhikari <pa1038@nyu.edu>
2024-04-28 05:47:22 +0545

    helloworld from axum

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,6 +1,21 @@
+use axum::{response::Html, routing::get, Router};
 use model::Game;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let game: Game = Game::new();
+
+    // build our application with a route
+    let app = Router::new().route("/", get(handler));
+
+    // run it
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
     println!("{:?}", game);
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1> helloworld!<h1>")
 }
