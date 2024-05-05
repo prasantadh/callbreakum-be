9a1ada783787049c812f84651bed142dbfb02dc7
Prasant Adhikari <pa1038@nyu.edu>
2024-05-06 05:42:34 +0545

    add a deck of cards

--- model/src/game.rs
+++ model/src/game.rs
@@ -1,8 +1,71 @@
+use rand::seq::SliceRandom;
+use rand::thread_rng;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
+#[derive(Debug, Serialize, Deserialize, Copy, Clone)]
+pub enum Rank {
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+}
+
+impl Rank {
+    fn all() -> Vec<Rank> {
+        return vec![Rank::Two, Rank::Three];
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, Copy, Clone)]
+pub enum Suit {
+    Clubs,
+    Diamonds,
+    Hearts,
+    Spades,
+}
+
+impl Suit {
+    fn all() -> Vec<Suit> {
+        vec![Suit::Clubs, Suit::Diamonds, Suit::Hearts, Suit::Spades]
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, Copy, Clone)]
+pub struct Card {
+    rank: Rank,
+    suit: Suit,
+}
+
 #[derive(Debug, Serialize, Deserialize)]
-enum Card {}
+pub struct Deck {
+    cards: Vec<Card>,
+}
+
+impl Deck {
+    pub fn new() -> Deck {
+        let mut deck = Deck { cards: vec![] };
+        for rank in Rank::all().iter() {
+            for suit in Suit::all().iter() {
+                deck.cards.push(Card {
+                    rank: *rank,
+                    suit: *suit,
+                })
+            }
+        }
+        // TODO shuffle in such a way that splitting 4-ways is callbreak valid
+        deck.cards.shuffle(&mut thread_rng());
+        return deck;
+    }
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Player {
@@ -73,5 +136,34 @@ impl Game {
                 }
             }
         };
+    }
+
+    pub fn start_round(&mut self) -> Result<(), &'static str> {
+        if self.rounds.len() == 5 {
+            return Err("the game is already over!");
+        }
+        if self.players.len() != 4 {
+            return Err("not enough players to start the round");
+        }
+        if self.rounds.last().is_none() {
+            self.rounds.push(Round {
+                tricks: vec![],
+                hands: vec![],
+                calls: vec![],
+            })
+        };
+        let mut new_round = Round {
+            tricks: vec![],
+            hands: vec![],
+            calls: vec![],
+        };
+        for _ in 0..4 {
+            new_round.hands.push(Hand { cards: vec![] })
+        }
+        let deck = Deck::new();
+        for i in 0..52 {
+            new_round.hands[i].cards.push(deck.cards[i]);
+        }
+        Ok(())
     }
 }
